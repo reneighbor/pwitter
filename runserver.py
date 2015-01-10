43eb7bcfd2b532c09fcd3b3cc90bc2285a6b4ee9
Renee Chu <renee@twilio.com>
2015-01-10 11:03:56 -0800

    Revert "first resource, models, db setup"
    This reverts commit 57642602bf4b04607d45fb002bf5e20c78f8e713.

--- runserver.py
+++
@@ -1,3 +0,0 @@
-#!flask/bin/python
-from service import app, resources
-app.run(debug=True)
--- service/resources/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask.ext.restful import Api, reqparse, fields, marshal
-
-from service import app
-from users_resource import UsersList
-
-api = Api(app)
-
-api.add_resource(UsersList, '/users')
